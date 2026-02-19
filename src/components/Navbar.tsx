e096ddb742ba3e33e33f3b84e76b868539600c11
MiamiPrécieu <230255@fppiramide.com>
2026-02-19 14:13:10 +0100

    Menú dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,11 +31,20 @@ export default function Navbar() {
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
           </div>
-          <ul
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Homepage</a></li>
-            <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
+          <ul className="menu dropdown-content bg-base-200 rounded-box w-56">
+            <li><a>Inicio</a></li>
+            <li>
+              <details open>
+                <summary>Perfumes</summary>
+                <ul>
+                  <li><a>Populares</a></li>
+                  <li><a>Por notas</a></li>
+                  <li><a>Marca</a></li>
+                </ul>
+              </details>
+            </li>
+            <li><a>Perfumista</a></li>
+            <li><button className="btn btn-sm btn-accent">PREMIUM</button></li>
           </ul>
         </div>
       </div>
