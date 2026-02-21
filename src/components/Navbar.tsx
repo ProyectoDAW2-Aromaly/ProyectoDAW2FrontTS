3a8e10e2c1d491fef8a9a3d07bbae2b15a1bbfbc
MiamiPrécieu <230255@fppiramide.com>
2026-02-21 12:29:30 +0100

    Dejarlo así o un modal, se puede modificar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,8 +20,8 @@ export default function Navbar() {
 
   // Ejecuta esto solo cuando el estado theme cambie
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme)
+      document.documentElement.setAttribute("data-theme", theme);
+      localStorage.setItem("theme", theme)
   }, [theme])
 
   const [showSearch, setShowSearch] = useState(false);
@@ -50,34 +50,40 @@ export default function Navbar() {
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl"><img src="/public/aromaly.png" alt="Logo marca" /></a>
+        <a className="btn btn-ghost text-xl"><img src="/public/aromaly.png" alt="Logo marca"/></a>
       </div>
       <div className="navbar-end">
         {showSearch && (
-          <input type="text" placeholder="Buscar..." className="input input-bordered ml-2 mr-2 w-48 transition-all duration-500 ease-out ${show}" />
+          <input 
+            type="text" 
+            placeholder="Buscar..."
+            autoFocus
+            className="input input-bordered ml-2 mr-2 w-48 transition-all duration-500 ease-out ${show}" 
+          />
         )}
-        <button
+        <button 
           className="btn btn-ghost btn-circle mr-3"
+          // Si showSearch es false, !showSearch es true, y del revés. Cambia el estado al valor opuesto.
           onClick={() => setShowSearch(!showSearch)}
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
         </button>
         <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-3">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-          </div>
-          <ul
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-1 w-30 p-2 shadow">
-            <li><a>Perfil</a></li>
-            <li><a>Ajustes</a></li>
-            <div className="divider"></div>
-            <li><button className="btn btn-xs btn-accent">PREMIUM</button></li>
-          </ul>
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-3">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
         </div>
+      </div>
+      <ul
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-1 w-30 p-2 shadow">
+        <li><a>Perfil</a></li>
+        <li><a>Ajustes</a></li>
+        <div className="divider"></div>
+        <li><button className="btn btn-xs btn-accent">PREMIUM</button></li>
+      </ul>
+      </div>
 
         <label className="toggle text-base-content mr-5">
           <input type="checkbox" onChange={handleToggle} checked={theme === THEMES.dark} className="theme-controller" />
