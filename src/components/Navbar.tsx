d0bd546011bed5ffbf7de4b484351981892c3bc0
MiamiPrécieu <230255@fppiramide.com>
2026-02-21 11:39:01 +0100

    Navbar casi terminado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,9 +20,11 @@ export default function Navbar() {
 
   // Ejecuta esto solo cuando el estado theme cambie
   useEffect(() => {
-      document.documentElement.setAttribute("data-theme", theme);
-      localStorage.setItem("theme", theme)
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme)
   }, [theme])
+
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -44,27 +46,40 @@ export default function Navbar() {
               </details>
             </li>
             <li><a>Perfumista</a></li>
-            <li><button className="btn btn-sm btn-accent">PREMIUM</button></li>
           </ul>
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl"><img src="/public/aromaly.png" alt="Logo marca" /></a>
       </div>
       <div className="navbar-end">
-        {/* mr -> Margin right */}
-        <button className="btn btn-ghost btn-circle mr-2">
+        {showSearch && (
+          <input type="text" placeholder="Buscar..." className="input input-bordered ml-2 mr-2 w-48 transition-all duration-500 ease-out ${show}" />
+        )}
+        <button
+          className="btn btn-ghost btn-circle mr-3"
+          onClick={() => setShowSearch(!showSearch)}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
         </button>
-        <button className="btn btn-ghost btn-circle avatar mr-3">
-          <div className="w-9 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-3">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
           </div>
-        </button>
+          <ul
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-1 w-30 p-2 shadow">
+            <li><a>Perfil</a></li>
+            <li><a>Ajustes</a></li>
+            <div className="divider"></div>
+            <li><button className="btn btn-xs btn-accent">PREMIUM</button></li>
+          </ul>
+        </div>
 
-        <label className="toggle text-base-content">
+        <label className="toggle text-base-content mr-5">
           <input type="checkbox" onChange={handleToggle} checked={theme === THEMES.dark} className="theme-controller" />
 
           <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
