b7d254bf2353c5a2936aac0b5ff8e2509ce21b56
MiamiPrécieu <230255@fppiramide.com>
2026-02-22 21:18:08 +0100

    Arreglado color svg claro/oscuro. Modificación de temas de DaisyUI.

--- src/views/perfume/PerfumePage.tsx
+++ src/views/perfume/PerfumePage.tsx
@@ -13,16 +13,16 @@ const PerfumePage = () => {
                         />
                     </figure>
                     <div className="card-body items-start">
-                        <h2 className="card-title ml-1">LIRA</h2>
-                        <button className="btn btn-ghost bg-[#FFF7ED] rounded-2xl"> {/*Habría que mirar qué hacer cuando es el tema oscuro*/}
+                        <h1 className="card-title ml-2">LIRA</h1>
+                        <button className="btn btn-ghost bg-[#FFF7ED] rounded-2xl self-start p-2 h-auto min-h-0"> {/*Habría que mirar qué hacer cuando es el tema oscuro*/}
                             <figure className="w-auto h-5">
                                 <img 
                                     src="/perfume-info/xerjoff-logo.png" 
-                                    alt="Logo de la marca" 
-                                    className="w-full h-full object-contain"
+                                    alt="Logo de la marca"
                                     />
                             </figure>
                         </button>
+                        <div className="divider">Descripción</div>
                         <p>Lira is a perfume whose every aspect enchants, from its deliciously tempting scent to its poetic origin story. 
                             Its name derives from the Italian word for lyre, the ancient musical instrument which holds great significance in mythology.
                             <br/>
@@ -30,11 +30,11 @@ const PerfumePage = () => {
                             Lira is based on a perfume originally released by Casamorati in the late 1800s that was created to help a beautiful, aspiring actress realise her dreams. 
                             It was intended to make a lasting impression on all those who encountered it, just like the sweet music of the lyre.
                         </p>
-                        <div className="divider">Info</div>
+                        <div className="divider">Información general</div>
                         <h5>Familia olfativa:
-                            <a href="" className="badge badge-xs badge-soft badge-accent ml-2">Oriental</a>
-                            <a href="" className="badge badge-xs badge-soft badge-accent ml-2">Floral</a>
-                            <a href="" className="badge badge-xs badge-soft badge-accent ml-2">Gourmand</a>
+                            <a href="" className="badge badge-xs badge-soft badge-neutral ml-2">Oriental</a>
+                            <a href="" className="badge badge-xs badge-soft badge-neutral ml-2">Floral</a>
+                            <a href="" className="badge badge-xs badge-soft badge-neutral ml-2">Gourmand</a>
                         </h5>
                         <h5 className="flex items-center">
                             Género 
@@ -42,8 +42,7 @@ const PerfumePage = () => {
                                 <img 
                                     src="/perfume-info/icons/genre/female-icon.svg" 
                                     alt="Icono de género"
-                                    className="w-7 ml-2"
-                                    color="green"
+                                    className="w-7 ml-2 icon-theme-aware"
                                 />
                             </figure>
                         </h5> {/* https://www.svgrepo.com/ https://allsvgicons.com/ svg gratis */}
@@ -51,10 +50,6 @@ const PerfumePage = () => {
                         <h5>Fecha de lanzamiento: 2011</h5>
 
                         {/* <p>Sus notas base son: Naranja roja, lavanda, bergamota (top). Canela, jazmin, rosa (corazón). Caramelo, almizcle, vainilla (base)</p> */}
-
-                        {/* <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Watch</button>
-                        </div> */}
                     </div>
                 </div>
             </div>
