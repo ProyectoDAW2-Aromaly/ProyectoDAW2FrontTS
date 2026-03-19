dc117ae9f311401c6d7336a972c3e0990c12cc61
MiamiPrécieu <230255@fppiramide.com>
2026-03-19 19:47:44 +0100

    Añadidos divs y comentarios para separar el código. He empezado las listas de los usuarios/

--- src/views/perfume/PerfumePage.tsx
+++ src/views/perfume/PerfumePage.tsx
@@ -82,6 +82,8 @@ const PerfumePage = () => {
                     </div>
                 </div>
 
+                {/* PIRÁMIDE OLFATIVA */}
+
                 <h1 className="text-2xl text-center mb-10 mt-10">PIRÁMIDE OLFATIVA</h1>
                 <div className="flex flex-wrap gap-12" >
                     {selectedPerfume.pyramids.map(pyramid =>
@@ -111,6 +113,8 @@ const PerfumePage = () => {
                         </div>
                     )}
                 </div>
+
+                {/* VALORACIONES */}
 
                 <h1 className="text-2xl text-center mb-10 mt-10">VALORACIONES</h1>
                 <div className="card bg-base-100 shadow-sm w-auto">
@@ -370,7 +374,149 @@ const PerfumePage = () => {
 
                 </div>
 
+                {/* LISTAS DESTACADAS */}
+
+                {/* Necesitamos nombre de usuario + icono, seguir la lista, título de la lista, iconos con los perfumes, y ¿época del año o mejor quitarla? y ver lista completa */}
+
+                <h1 className="text-2xl text-center mb-10 mt-10">LISTAS DESTACADAS</h1>
+                <div className="flex flex-wrap gap-12" >
+
+                    <div className="card bg-base-100 shadow-sm w-96">
+                        <div className="card-body">
+
+                            <div className="flex flex-wrap gap-6 mb-4">
+                                <div className="avatar">
+                                    <div className="w-14 rounded-full">
+                                        <img src="public\perfume-info\perfume\lira\xerjoff-lira.jpg" />
+                                    </div>
+                                </div>
+                                <h2 className="card-title">Mis perfumes de verano</h2>
+                                {/* <button className="btn btn-primary">Guardar lista</button> */}
+                            </div>
+
+                            <div className="divider"></div>
+                            <div className="divider divider-neutral"></div>
+
+                            <div className="avatar-group flex justify-center -space-x-2">
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
+                                    </div>
+                                </div>
+                                <div className="avatar avatar-placeholder">
+                                    <div className="bg-neutral text-neutral-content w-12">
+                                        <span>+99</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="btn btn-neutral">Ver lista</button>
+
+                        </div>
+                    </div>
+                    <div className="card bg-base-100 shadow-sm w-96">
+                        <div className="card-body">
+
+                            <div className="flex flex-wrap gap-6 mb-4">
+                                <div className="avatar">
+                                    <div className="w-14 rounded-full">
+                                        <img src="public\perfume-info\perfume\lira\xerjoff-lira.jpg" />
+                                    </div>
+                                </div>
+                                <h2 className="card-title">Mejores perfumes nicho</h2>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="card bg-base-100 shadow-sm w-96">
+                        <div className="card-body">
+
+                            <div className="flex flex-wrap gap-6 mb-4">
+                                <div className="avatar">
+                                    <div className="w-14 rounded-full">
+                                        <img src="public\perfume-info\perfume\lira\xerjoff-lira.jpg" />
+                                    </div>
+                                </div>
+                                <h2 className="card-title">Mis favoritos unisex</h2>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* PERFUMES SIMILARES */}
+
+                {/* Card con imagen del perfume, logo + nombre, género, notas base y ¿época del año? + botón de ver perfume */}
+
+                <h1 className="text-2xl text-center mb-10 mt-10">PERFUMES SIMILARES</h1>
+                <div className="flex flex-wrap gap-12" >
+                    {selectedPerfume.pyramids.map(pyramid =>
+                        <div className="card bg-base-100 shadow-sm w-96" key={pyramid.category}>
+                            <div className="card-body">
+                                <h2 className="card-title">{pyramid.category}</h2>
+
+                                <div className="flex flex-wrap gap-6 mb-4">
+                                    {pyramid.notes.map(note =>
+                                        note.imageSrc ?
+                                            <div className="avatar" key={note.name}>
+                                                <div className="w-14 rounded-full">
+                                                    <img src={note.imageSrc} />
+                                                </div>
+                                            </div>
+                                            : null
+                                    )}
+                                </div>
+
+                                <div className="flex flex-wrap gap-2">
+                                    {pyramid.notes.map(note =>
+                                        <a key={note.name} href="" className="badge badge-s badge-soft badge-neutral">{note.name}</a>
+                                    )}
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+                </div>
+
+                {/* COMENTARIOS */}
+
+                {/* Necesito iconos varios + foto de perfil + nombre + descripción del perfume + ranking estrellas si lo hay + botón de responder */}
+
+                {/* FIXME: Esto fue una prueba, como idea, obviamente no está terminado */}
+
+                <h1 className="text-2xl text-center mb-10 mt-10">COMENTARIOS</h1>
+                <div className="card bg-base-100 shadow-sm w-auto" >
+                    <li className="flex list-row list-none">
+                        <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp" /></div>
+                        <div>
+                            <div>Dio Lupa</div>
+                            <div className="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
+                        </div>
+                        <p className="list-col-wrap text-xs">
+                            "Remaining Reason" became an instant hit, praised for its haunting sound and emotional depth. A viral performance brought it widespread recognition, making it one of Dio Lupa’s most iconic tracks.
+                        </p>
+                        <button className="btn btn-square btn-ghost">
+                            <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+                        </button>
+                        <button className="btn btn-square btn-ghost">
+                            <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+                        </button>
+                    </li>
+                </div>
+
             </div>
+            {/* Cuando exista */}
+            {/* <Footer /> */}
         </div>
     )
 }
