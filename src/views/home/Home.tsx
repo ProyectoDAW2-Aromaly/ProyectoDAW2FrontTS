26a263a69d5933e31d7779086b1160d2daa7b2c2
Amina <240034@fppiramide.com>
2026-03-21 02:37:56 +0100

    setup home

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,10 +1,30 @@
-import Navbar from "../../components/Navbar"
+//still editing not the last version
 
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 export default function Home() {
-    return(
-        <div>
-            <Navbar/>
-            <h1>Esta es la home page de Aromaly</h1>
+  return (
+    <div>
+      <Navbar />
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(../../../public/home-imgs/hero.png)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-4xl font-bold">El universo del perfume, en un solo lugar</h1>
+            <p className="mb-5">
+              Descubre, vota y comparte tus perfumes favoritos
+            </p>
+            <button className="btn btn-primary">Explorar ahora</button>
+          </div>
         </div>
-    )
+      </div>
+      <Footer/>
+    </div>
+  );
 }
