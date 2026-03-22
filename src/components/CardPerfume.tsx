c82d53317b606ba4d4341ecd2b55826d8332c75f
MiamiPrécieu <230255@fppiramide.com>
2026-03-22 17:35:55 +0100

    Añadido tooltip guardar en listas y guardar en favoritos. Falta cuadrar los colores.

--- src/components/CardPerfume.tsx
+++ src/components/CardPerfume.tsx
@@ -1,1 +1,25 @@
-// Esto es para las tarjetas de los perfumes porque será reutilizable
+<div className="card bg-base-100 w-96 shadow-sm rounded-2xl overflow-hidden group">
+    <img
+        className="w-full h-80 object-cover"
+        src="/perfume-info/perfume/born-in-roma/valentino-born-in-roma-intense-donna.jpg"
+        alt="Perfume recomendado Born in Roma Intense Donna"
+    />
+
+    <div className="absolute bottom-0 w-full h-28 bg-black/70 transition-all duration-300 group-hover:h-3/4">
+
+        <div className="flex flex-col justify-center items-center p-4 text-white">
+            <h2 className="text-lg font-bold">BORN IN ROMA INTENSE DONNA</h2>
+            <a href="">Valentino</a>
+        </div>
+
+        <div className="absolute bottom-[1/3] w-full h-1/2 flex flex-col justify-between items-center text-center gap-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-6">
+            <div className="flex flex-wrap justify-center gap-2">
+                <a href="" className="badge badge-soft">Oriental</a>
+                <a href="" className="badge badge-soft">Floral</a>
+                <a href="" className="badge badge-soft">Gourmand</a>
+            </div>
+            <button className="btn btn-primary mt-2">Ver perfume</button>
+        </div>
+
+    </div>
+</div>
--- src/views/perfume/PerfumePage.tsx
+++ src/views/perfume/PerfumePage.tsx
@@ -600,14 +600,42 @@ const PerfumePage = () => {
                 <h1 className="text-2xl text-center mb-10 mt-10">PERFUMES SIMILARES</h1>
                 <div className="flex flex-wrap gap-12" >
 
-                    <div className="card bg-base-100 w-96 shadow-sm rounded-2xl overflow-hidden group">
-                        <img
+                    <div className="card bg-base-100 w-96 shadow-sm rounded-2xl relative group">
+
+                        {/* z-50 -> Profundidad. Cuanto + número, + arriba */}
+                        <div className="tooltip save absolute top-2 right-14 z-50" data-tip="Guardar en favoritos">
+                            <button className="btn btn-circle btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.6em]">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <div className="tooltip save absolute top-2 right-2 z-50" data-tip="Guardar en lista">
+                            <button className="btn btn-circle btn-accent">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="2.5"
+                                    stroke="currentColor"
+                                    className="size-[1.6em]"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <div className="relative rounded-2xl overflow-hidden">
+                            <img
                             className="w-full h-80 object-cover"
                             src="/perfume-info/perfume/born-in-roma/valentino-born-in-roma-intense-donna.jpg"
                             alt="Perfume recomendado Born in Roma Intense Donna"
                         />
+                        </div>
+                        
 
-                        <div className="absolute bottom-0 w-full h-28 bg-black/70 transition-all duration-300 group-hover:h-3/4">
+                        <div className="absolute bottom-0 w-full rounded-b-2xl h-28 bg-black/70 transition-all duration-300 group-hover:h-3/4">
 
                             <div className="flex flex-col justify-center items-center p-4 text-white">
                                 <h2 className="text-lg font-bold">BORN IN ROMA INTENSE DONNA</h2>
