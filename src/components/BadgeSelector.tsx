9f070472440144f7703798da65871b2dd412bc2f
MiamiPrécieu <230255@fppiramide.com>
2026-04-03 18:07:31 +0200

    Formulario hecho. Se han modificado algunas cosas para que el estilo visual coincida.

--- src/components/BadgeSelector.tsx
+++ src/components/BadgeSelector.tsx
@@ -1,65 +1,53 @@
 import React, { useState } from "react";
 
 type Props = {
-  items: string[];
-  label: string;
+    items: string[];
+    label: string;
 };
 
 export const BadgeSelector = ({ items, label }: Props) => {
-  const [selectedItems, setSelectedItems] = useState<string[]>([]);
-//   const [selectedFamilies, setSelectedFamilies] = useState<string[]>([]);
+    const [selectedItems, setSelectedItems] = useState<string[]>([]);
 
-  const toggle = (item: string) => {
-    if (selectedItems.includes(item)) {
-        // Quita del array la nota que acabamos de deseleccionar
-        // "item" es cada nota del array, si no es la que queremos quitar, se queda
-        const newSelectedItems = selectedItems.filter(it => it !== item);
-        setSelectedItems(newSelectedItems);
-    } else {
-        setSelectedItems([...selectedItems, item]);
-    }
-  };
+    const toggle = (item: string) => {
+        if (selectedItems.includes(item)) {
+            // Quita del array la nota que acabamos de deseleccionar
+            // "item" es cada nota del array, si no es la que queremos quitar, se queda
+            const newSelectedItems = selectedItems.filter(it => it !== item);
+            setSelectedItems(newSelectedItems);
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
 
-//   const toggleFamily = (family: string) => {
-//     if (selectedFamilies.includes(family)) {
-//         // Quita del array la familia que acabamos de deseleccionar
-//         // El filtro recibe una función que ejecuta por cada elemento del array (item) y si es true/false, mete el objeto en un nuevo array newSelectedFamilies
-//         // item -> Cada item de la lista.
-//         const newSelectedFamilies = selectedFamilies.filter(item => item !== family);
-//         setSelectedFamilies(newSelectedFamilies);
-//     } else {
-//         setSelectedFamilies([...selectedFamilies, family]);
-//     }
-//   };
     return (
         <div className="dropdown mb-4">
-              <label tabIndex={0} className="btn w-full justify-start h-auto py-2">
+            <label tabIndex={0} className="w-full flex flex-wrap gap-2 justify-start h-auto rounded-field cursor-pointer py-2 bg-transparent border-2 border-neutral/20 pl-2">
                 <div className="flex flex-wrap gap-2">
-                  {selectedItems.length > 0
-                  ? selectedItems.map(item => (
-                      <span key={item} className="badge badge-primary mr-1">{item}</span>
-                    ))
-                  : label}
+                    {selectedItems.length > 0
+                        ? selectedItems.map(item => (
+                            <span key={item} className="badge badge-sm badge-neutral text-primary-content ml-1">{item}</span>
+                        ))
+                        : label}
                 </div>
-              </label>
-              <ul
+            </label>
+            <ul
                 tabIndex={0}
                 className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-full max-h-60 overflow-y-auto"
-              >
+            >
                 {items.map(item => (
-                  <li key={item}>
-                    <label className="cursor-pointer flex items-center gap-2">
-                      <input
-                        type="checkbox"
-                        checked={selectedItems.includes(item)}
-                        onChange={() => toggle(item)}
-                        className="checkbox checkbox-primary"
-                      />
-                      {item}
-                    </label>
-                  </li>
+                    <li key={item}>
+                        <label className="cursor-pointer flex items-center gap-2">
+                            <input
+                                type="checkbox"
+                                checked={selectedItems.includes(item)}
+                                onChange={() => toggle(item)}
+                                className="checkbox checkbox-primary"
+                            />
+                            {item}
+                        </label>
+                    </li>
                 ))}
-              </ul>
-            </div>
+            </ul>
+        </div>
     )
 }
--- src/views/perfume_form/PerfumeForm.tsx
+++ src/views/perfume_form/PerfumeForm.tsx
@@ -5,15 +5,27 @@ const availableNotes = [
   'Rosa', 'Jazmin', 'Naranja roja', 'Lavanda', 'Canela', 'Caramelo'
 ];
 
-const availanbleOlfatoryFamily = [
+const availableOlfatoryFamily = [
   'Cítrico', 'Oriental', 'Floral', 'Gourmand', 'Amaderado'
 ];
 
+const genre = [
+  'Female', 'Male', "Unisex"
+]
+
+const availablePerfumer = [
+  'Chris Maurice', 'Honorine Blanc', 'Amadine Clerc-Marie', 'Quentin Bisch'
+]
+
+const availableColecction = [
+  'Sin colección', 'Born in Roma', 'Coleccion 1', 'Colección 2'
+]
 
 export default function PerfumeForm() {
   return (
-    <div className="hero bg-base-200 min-h-screen flex justify-center items-start py-10">
-      <div className="card bg-base-100 w-95 shrink-0 shadow-2xl mt-10">
+    // ! py -> Padding vertical. px -> Padding horizontal. Lo mismo con mx y my pero con margin.
+    <div className="hero bg-base-200 min-h-screen flex justify-center items-start py-20">
+      <div className="card bg-base-100 w-95 shrink-0 shadow-2xl">
         <div className="card-body">
           <div className="flex justify-center">
             <h1 className="font-semibold text-lg">FORMULARIO PERFUME</h1>
@@ -21,10 +33,10 @@ export default function PerfumeForm() {
           
           <fieldset className="fieldset">
             
-            <label className="label text-neutral font-semibold">NOMBRE</label>
+            <label className="label text-neutral font-semibold">Nombre</label>
             <input type="text" className="input" placeholder="Nombre" />
 
-            <span className="label text-neutral font-semibold">MARCA</span>
+            <span className="label text-neutral font-semibold">Marca</span>
             <label className="select">
               <select>
                 <option disabled selected>Selecciona una marca</option>
@@ -34,38 +46,76 @@ export default function PerfumeForm() {
               </select>
             </label>
 
-            <label className="label text-neutral font-semibold">DESCRIPCIÓN</label>
+            <label className="label text-neutral font-semibold">Descripción</label>
             <textarea className="textarea" placeholder="Descripción"></textarea>
 
-            <div className="divider">Información general</div>
+            <div className="divider font-semibold">INFORMACIÓN GENERAL</div>
+
+            {/* Familias olfativas */}
+            <label className="label text-neutral font-semibold">Familia Olfativa</label>
+            <BadgeSelector
+              items={availableOlfatoryFamily}
+              label="Selecciona las familias olfativas"
+            />
+
+            <span className="label text-neutral font-semibold">Género</span>
+            <label className="select">
+              <select defaultValue="">
+                <option disabled value="">
+                  Selecciona un género
+                </option>
+
+                {genre.map(g => (
+                  <option key={g} value={g}>
+                    {g}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            <label className="label text-neutral font-semibold">Perfumista</label>
+            <BadgeSelector
+              items={availablePerfumer}
+              label="Selecciona los perfumistas"
+            />
+
+            <label className="label text-neutral font-semibold">Fecha de lanzamiento</label>
+            <input type="number" className="input" placeholder="Año de lanzamiento" min={1800} max={new Date().getFullYear()} />
+
+            <span className="label text-neutral font-semibold">Colección</span>
+            <label className="select">
+              <select defaultValue="">
+                <option disabled value="">
+                  Selecciona una colección
+                </option>
+
+                {availableColecction.map(colecction => (
+                  <option key={colecction} value={colecction}>
+                    {colecction}
+                  </option>
+                ))}
+              </select>
+            </label>
 
             {/* Notas */}
-            <h2 className="font-semibold">NOTAS</h2>
+            <div className="divider font-semibold">NOTAS</div>
             
-            <label className="label text-neutral">Salida</label>
+            <label className="label text-neutral font-semibold">Salida</label>
             <BadgeSelector
               items={availableNotes}
               label="Selecciona las notas de salida"
             />
 
-            <label className="label text-neutral">Corazón</label>
+            <label className="label text-neutral font-semibold">Corazón</label>
             <BadgeSelector
               items={availableNotes}
               label="Selecciona las notas corazón"
             />
 
-            <label className="label text-neutral">Base</label>
+            <label className="label text-neutral font-semibold">Base</label>
             <BadgeSelector
               items={availableNotes}
               label="Selecciona las notas base"
-            />
-            
-
-            {/* Familias olfativas */}
-            <label className="label text-neutral font-semibold">FAMILIAS OLFATIVAS</label>
-            <BadgeSelector
-              items={availanbleOlfatoryFamily}
-              label="Selecciona la familia olfativa"
             />
 
             <button className="btn btn-neutral mt-2 hover:btn-accent text-primary-content">
